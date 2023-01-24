8577a73b6ccef8e67c9da9c62baac8788831f61b
Gopi Krishna <awxsoul8@gmail.com>
2023-01-24 22:11:17 +0530

    text_ques.py created
    Question created using OpenAI from given text

--- text_ques.py
+++ text_ques.py
@@ -2,9 +2,34 @@ import openai
 
 openai.api_key="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"
 
+content="Hormonal changes are also a major cause of why your hair may start thinning at a young age. Note that there are two phases of hair growth"
 
-text="Codex understands dozens of different programming languages. Many share similar conventions for comments, functions and other programming syntax. By specifying the language and what version in a comment, Codex is better able to provide a completion for what you want. That said, Codex is fairly flexible with style and syntax."
-prompt="Generate 5 questions from the given tex '"+ text +"'"
-response= openai.Completion.create(engine="text-davinci-001",prompt=prompt,max_tokens=1000)
+prompt="Generate 5 questions from the given text '"+ content +"'"
+response= openai.Completion.create(engine="text-davinci-001",prompt=prompt, max_tokens=1000)
 
+# print(response["choices"][0]["text"])
+
+#gets only the text from the output
+questions=response["choices"][0]["text"]
 print(response)
+ques_list=questions.split("\n")
+#to seperate the numbers
+
+
+#remove numbering
+for i in range(2,7):
+    ques_list[i]=ques_list[i].split(".")[1]
+for i in range(2,7):
+    
+
+    prompt= "Answer this question in one sentence,"+ques_list[i]
+    response= openai.Completion.create(engine="text-davinci-001",prompt=prompt, max_tokens=100)
+    answer=response["choices"][0]["text"]
+    
+    print("Q: ",ques_list[i])
+    print("ans: ",answer,"\n")
+
+    
+
+
+
